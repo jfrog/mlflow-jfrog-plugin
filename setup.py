a3be2902a014903f04474456fbdd16d7f66c72fe
carmith <carmith@jfrog.com>
2024-03-10 08:56:22 +0200

    Added project long description

--- setup.py
+++ setup.py
@@ -2,8 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlflow-jfrog-plugin",
-    version="1.0.0",
+    version="1.0.1",
     description="Plugin to store MLFlow experiments artifacts on JFrog Artifactory",
+    long_description="JFrog MLFlow plugin is a plugin created by JFrog for customers using MLflow product. "
+                     "MLflow is an open-source platform for managing the end-to-end machine learning lifecycle. "
+                     "The JFrog MLflow plugin extends MLflow functionality by replacing the default artifacts location of MLflow with JFrog Artifactory. "
+                     "Once MLflow experiments artifacts are available inside JFrog Artifactory, "
+                     "they become an integral part of the company's release lifecycle as any other artifact and are also covered by all the security tools provided through the JFrog platform.",
     packages=find_packages(),
     install_requires=["mlflow", "requests>=2.31.0"],
     entry_points={
